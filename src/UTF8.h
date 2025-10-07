345984ec8d267434058d55947fc4f3cc5062abc9
Leon M. Busch-George <leon@georgemail.eu>
2025-10-07 20:21:58 +0200

    remove obsolete functions

--- src/UTF8.h
+++ src/UTF8.h
@@ -3,11 +3,6 @@
 
 #include <stdbool.h>
 
-size_t get_utf8_char_count(const void *buf, size_t bytecount);
-
-
-size_t get_utf8_byte_count(const void *buf, size_t charcount);
-
 enum CONVERSION_ERROR {
 	CE_OK = 0,
 	CE_ICONV_OPEN_ERRNO,
